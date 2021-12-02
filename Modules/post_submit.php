262615c0aa28f44a2d6b2564afadbf70cdda0aca
Shiki-Himura <athaus@hotmail.de>
2021-12-02 15:17:06 +0100

    update onclick

--- Modules/post_submit.php
+++ Modules/post_submit.php
@@ -2,7 +2,7 @@
     $user_post = "  <div>
                         <textarea id='post-description' class='text-white bg-dark' placeholder='Here you can enter a comment!' type='text' cols='50' rows='5'></textarea>
                         <div>
-                            <button id='index_submit' onclick='javascript:clearText();' class='btn btn-primary'>Submit</button>
+                            <button id='index_submit' class='btn btn-primary'>Submit</button>
                         </div>
                     </div>";
     if(isset($_SESSION['valid_user']))
