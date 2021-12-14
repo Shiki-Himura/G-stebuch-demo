6af3a957856728e833cfdb19b44dbceaf2647d75
Shiki-Himura <athaus@hotmail.de>
2021-12-14 15:26:23 +0100

    update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -1,8 +1,19 @@
 <?php include_once "Modules/header.php" ?>
+<?php
+    //TODO: finish styling userprofile; Add name and post counter from THAT user
+    $userprofile = "
+    <div class='d-flex flex-column mt-5 text-white'>
+        <h1><u>Profile</u></h1>
+        <div class='fs-4'>Username: </div>
+        <div class='fs-4'>Posts: </div>
+    </div>";
 
-<div>
-    <h1>Profile</h1>
-    
-</div>
-
+    if(isset($_SESSION['valid_user']))
+        echo($userprofile);
+    else
+    {
+        header("Location: login.php");
+        exit();
+    }
+?>
 <?php include_once "Modules/footer.php" ?>
