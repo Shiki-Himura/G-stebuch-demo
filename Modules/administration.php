ae0f293a020d812b37a6c8381c838016b781e806
Shiki-Himura <athaus@hotmail.de>
2022-01-05 15:25:01 +0100

    update capitalization

--- Modules/administration.php
+++ Modules/administration.php
@@ -5,8 +5,6 @@
     }
 
     if(isset($_SESSION['valid_user']) && $_SESSION['valid_user'] == 'Admin')
-    {
         include_once "Logic/ContentManager.php";
-    }
     // TODO: add admin page content to change different settings
 ?>
